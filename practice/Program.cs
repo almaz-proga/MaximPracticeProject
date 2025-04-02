edd5524c92f1efed97d07f8f398a86aa5b032436
almaz-proga <almaz.g05@gmail.com>
2025-04-02 22:44:29 +0300

    Первый коммит - добавил задание 1

---
+++ practice/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace practice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите строку: ");
+            string word = Console.ReadLine();
+            string newword = null;
+            if (word.Length % 2 == 0)
+            {
+                
+                for(int i = word.Length / 2 - 1; i >= 0; i--)
+                {
+                    newword += word[i];
+                }
+                for(int i = word.Length - 1; i >= word.Length / 2; i--)
+                {
+                    newword += word[i];
+                }
+            }
+            else
+            {
+                for(int i = word.Length - 1; i >= 0; i--)
+                {
+                    newword += word[i];
+                }
+                newword += word;
+            }
+            Console.WriteLine(newword);
+            Console.ReadLine();
+        }
+    }
+}
